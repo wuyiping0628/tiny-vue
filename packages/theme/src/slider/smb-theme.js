d3b9440fdcde59f602f7a5de04d95948e99c0e3e
wuyiping0628 <1106773985@qq.com>
2023-12-21 01:26:40 -0800

    feat(slider):[slider] adapt to slider smb themes

--- packages/theme/src/slider/smb-theme.js
+++ packages/theme/src/slider/smb-theme.js
@@ -1,1 +1,17 @@
-export const tinySliderSmbTheme = {}
+export const tinySliderSmbTheme = {
+  'ti-slider-range-bg-color': 'var(--ti-base-color-brand-2)',
+  'ti-slider-range-hover-bg-color': 'var(--ti-base-color-brand-2)',
+  'ti-slider-handle-border-color-hover': 'var(--ti-base-color-brand-2)',
+  'ti-slider-range-height': 'var(--ti-common-scrollbar-width)',
+  'ti-slider-handle-width': 'var(--ti-common-border-radius-6)',
+  'ti-slider-handle-border-radius': 'var(--ti-common-border-radius-4)',
+  'ti-slider-handle-border-color': 'var(--ti-base-color-brand-2)',
+  'ti-slider-handle-icon-fill-color': 'var(--ti-base-color-brand-2)',
+  'ti-slider-border-radius': 'var(--ti-common-border-radius-1)',
+  'ti-slider-bg-color': 'rgba(0,0,0,0.05)',
+  'ti-slider-range-disabled-bg-color': 'var(--ti-base-color-brand-5)',
+  'ti-slider-range-margin-top': 'var(--ti-common-space-0)',
+  'ti-slider-tips-text-color': 'var(--ti-base-color-common)',
+  'ti-slider-input-margin-left': '16px',
+  'ti-slider-handle-height': 'var(--ti-common-size-height-small)'
+}
